057731e69c48aceef2d0719a9e18d0afd477872e
nachodoria <ignaciodoriaoberman@gmail.com>
2024-07-25 16:14:38 -0600

    Changed Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,9 @@ export default function RootLayout({ children, }: Readonly<{ children: React.Rea
     <html lang="en" className={`${cormorant_Garamond.variable} ${monosans.variable} scroll-smooth`} >
       <meta name='viewport' content='width=device-width,initial-scale=1'></meta>
       <head>
-        <link rel="icon" href="/images/favicon.svg" />
+      <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🚀</text></svg>"></link>      
       </head>
-        <body className="bg-background">{children}</body>
+      <body className="bg-background">{children}</body>
     </html>
   );
 }
